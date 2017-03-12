86bdc8f35b993403ac999d5aee6c6701d0398d9f
Helder Ferreira <woounn@gmail.com>
2017-03-12 00:58:55 +0000

    Fix swap numbers and add land layout

--- app/src/main/java/com/example/helde/trabalho1/Fourth.java
+++ app/src/main/java/com/example/helde/trabalho1/Fourth.java
@@ -17,22 +17,28 @@ public class Fourth extends AppCompatActivity {
         Button btn1 = (Button) findViewById(R.id.botaoMedida1);
         Button btn2 = (Button) findViewById(R.id.botaoMedida2);
 
-        btn1.setText(getString(R.string.m));
+        btn1.setText(getString(R.string.inch));
         btn2.setText(getString(R.string.cm));
+
+
+        EditText tvToPrint = (EditText) findViewById(R.id.printTV);
+        tvToPrint.setKeyListener(null);
     }
 
     public void btnCalcular(View v) {  //Enters on click on calcular button
         Button btn1 = (Button) findViewById(R.id.botaoMedida1);
         Button btn2 = (Button) findViewById(R.id.botaoMedida2);
 
-        if (btn1.getText().toString().equals("m")) {
-            calculateByMeters();
+        if (btn1.getText().toString().equals("inch")) {
+            calculateByInches();
         } else {
-            Log.d("Entrei", "btnCalcular: naoEntrei");
+            if (btn1.getText().toString().equals("cm")) {
+                calculateByCentimeters();
+            }
         }
     }
 
-    private void calculateByMeters() {
+    private void calculateByInches() {
         Button btn2 = (Button) findViewById(R.id.botaoMedida2);
 
         EditText e1 = (EditText) findViewById(R.id.InsertN);
@@ -43,15 +49,31 @@ public class Fourth extends AppCompatActivity {
             valor = Float.parseFloat(v1);
 
             if (btn2.getText().toString().equals("cm")) {
-                printNumbers(Float.toString((valor * 1000)));
+                printNumbers(Double.toString((valor * 2.54)));
+            }
+
+        }
+    }
+
+    private void calculateByCentimeters() {
+        Button btn2 = (Button) findViewById(R.id.botaoMedida2);
+
+        EditText e1 = (EditText) findViewById(R.id.InsertN);
+        String v1 = e1.getText().toString();
+        float valor;
+
+        if (v1 != null && !v1.isEmpty()) {
+            valor = Float.parseFloat(v1);
+
+            if (btn2.getText().toString().equals("inch")) {
+                printNumbers(Double.toString((valor / 2.54)));
             }
 
         }
     }
 
     private void printNumbers(String number) {
-        Log.d("Entrei", "printNumbers: "+number);
-        TextView tvToPrint = (TextView) findViewById(R.id.printTV);
+        EditText tvToPrint = (EditText) findViewById(R.id.printTV);
 
         double nReceived = Double.parseDouble(number);
         double fractionalPart = nReceived % 1;
@@ -62,5 +84,39 @@ public class Fourth extends AppCompatActivity {
         } else {
             tvToPrint.setText(Integer.toString((int)integralPart));
         }
+    }
+
+    public void switchMesures(View v) {
+        Button btn1 = (Button) findViewById(R.id.botaoMedida1); // Change buttons text
+        Button btn2 = (Button) findViewById(R.id.botaoMedida2);
+
+        String stringBtn1 = btn1.getText().toString();
+        String stringBtn2 = btn2.getText().toString();
+
+        String aux1 = stringBtn1;
+
+        btn1.setText(stringBtn2);
+        btn2.setText(aux1);
+
+        /*if (stringBtn2.equals("inch")) {
+            calculateByInches();
+        } else {
+            if (stringBtn2.equals("cm")) {
+                calculateByCentimeters();
+            }
+        }*/
+
+        EditText e1 = (EditText) findViewById(R.id.InsertN); // Change EditText text
+        EditText e2 = (EditText) findViewById(R.id.printTV);
+
+        String srtE1 = e1.getText().toString();
+        String srtE2 = e2.getText().toString();
+
+        aux1 = srtE1;
+
+        e1.setText(srtE2);
+        e2.setText(aux1);
+
+
     }
 }
